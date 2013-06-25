f338f6ee675dc7ef8b52a6ff11eaadfb6331a29e
Paul Mietz Egli <paul@obscure.com>
2013-06-25 08:54:50 -0700

    switched to http://code.google.com/p/basic-http-client as it seems possible to adapt it for streaming

--- mobile/android/src/appersonlabs/iobridge/DeviceProxy.java
+++ mobile/android/src/appersonlabs/iobridge/DeviceProxy.java
@@ -1,45 +1,80 @@
 package appersonlabs.iobridge;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.http.entity.StringEntity;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollObject;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.util.TiConvert;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Build;
-import android.util.Log;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import com.turbomanage.httpclient.AsyncCallback;
+import com.turbomanage.httpclient.HttpResponse;
+import com.turbomanage.httpclient.ParameterMap;
+import com.turbomanage.httpclient.android.AndroidHttpClient;
 
 @Kroll.proxy(creatableInModule = AppersonlabsIoBridgeModule.class)
 public class DeviceProxy extends KrollProxy {
 
-    private static final String    BASE_URL                 = "http://api.realtime.io/v1";
+    private static class KrollAsyncCallback extends AsyncCallback {
 
-    private static AsyncHttpClient HTTP_CLIENT              = new AsyncHttpClient();
+        private KrollFunction callback;
 
-    private static final String    LCAT                     = "DeviceProxy";
+        private KrollObject   thisObject;
 
-    private static final String    PATH_CONNECTION_STATE    = "gateway/request/state";
+        public KrollAsyncCallback(KrollObject thisObject, KrollFunction callback) {
+            this.thisObject = thisObject;
+            this.callback = callback;
+        }
 
-    private static final String    PATH_READ_GPIO_REGISTER  = "gateway/request/register/read";
+        @Override
+        public void onComplete(HttpResponse httpResponse) {
+            if (callback != null) {
+                try {
+                    JSONObject obj = new JSONObject(httpResponse.getBodyAsString());
+                    callback.callAsync(thisObject, new Object[] { null, KrollDict.fromJSON(obj) });
+                }
+                catch (JSONException e) {
+                    onError(e);
+                }
+            }
+        }
 
-    private static final String    PATH_SEND_DATA           = "gateway/send";
+        @Override
+        public void onError(Exception e) {
+            if (callback != null) {
+                KrollDict err = new KrollDict();
+                err.put("message", e.getMessage());
+                callback.callAsync(thisObject, new Object[] { err, null });
+            }
+        }
+    }
+    
+    private static final String      BASE_URL                 = "http://api.realtime.io/v1/";
 
-    private static final String    PATH_WRITE_GPIO_REGISTER = "gateway/request/register/write";
+    private static AndroidHttpClient HTTP_CLIENT              = new AndroidHttpClient(BASE_URL);
 
-    private String                 apikey                   = null;
+    private static final String      LCAT                     = "DeviceProxy";
 
-    private String                 serial                   = null;
+    private static final String      PATH_CONNECTION_STATE    = "gateway/request/state";
+
+    private static final String      PATH_READ_GPIO_REGISTER  = "gateway/request/register/read";
+
+    private static final String      PATH_SEND_DATA           = "gateway/send";
+
+    private static final String      PATH_STREAMING           = "stream";
+
+    private static final String      PATH_WRITE_GPIO_REGISTER = "gateway/request/register/write";
+
+    private String                   apikey                   = null;
+
+    private String                   serial                   = null;
 
     public DeviceProxy() {
         disableConnectionReuseIfNecessary();
@@ -53,15 +88,11 @@ public class DeviceProxy extends KrollProxy {
     }
 
     @Kroll.method(name = "fetchConnectionState")
-    public void fetchConnectionState(final KrollFunction callback) {
+    public void fetchConnectionState(@Kroll.argument(optional = true) final KrollFunction callback) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("apikey", apikey);
         params.put("serial", serial);
         sendGETRequest(PATH_CONNECTION_STATE, params, callback);
-    }
-
-    private String getAbsoluteURL(String path) {
-        return BASE_URL + (path.startsWith("/") ? "" : "/") + path;
     }
 
     @Override
@@ -76,8 +107,21 @@ public class DeviceProxy extends KrollProxy {
         HTTP_CLIENT.addHeader("Content-Type", "application/json");
     }
 
+    @Override
+    public void onHasListenersChanged(String event, boolean hasListeners) {
+        if ("stream".equals(event)) {
+            if (hasListeners) {
+                // TODO start streaming
+            }
+            else {
+                // TODO stop streaming
+            }
+        }
+        super.onHasListenersChanged(event, hasListeners);
+    }
+
     @Kroll.method(name = "readGPIORegister")
-    public void readGPIORegister(int channel, String registerName, KrollFunction callback) {
+    public void readGPIORegister(int channel, String registerName, @Kroll.argument(optional = true) KrollFunction callback) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("serial", serial);
         params.put("channel", String.valueOf(channel));
@@ -85,59 +129,37 @@ public class DeviceProxy extends KrollProxy {
         sendPOSTRequest(PATH_READ_GPIO_REGISTER, params, callback);
     }
 
+    @Kroll.method(name = "sendData")
+    public void sendData(int channel, String payload, @Kroll.argument(optional = true) String encoding, @Kroll.argument(optional = true) KrollFunction callback) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("channel", String.valueOf(channel));
+        params.put("serial", serial);
+        params.put("encoding", encoding != null ? encoding : "plain");
+        params.put("payload", payload);
+        sendPOSTRequest(PATH_SEND_DATA, params, callback);
+    }
+
     private void sendGETRequest(final String relativePath, final Map<String, String> params, final KrollFunction callback) {
-        RequestParams requestParams = params != null ? new RequestParams(params) : new RequestParams();
-        requestParams.put("apikey", apikey);
-        requestParams.put("serial", serial);
+        ParameterMap paramMap = HTTP_CLIENT.newParams();
+        paramMap.put("apikey", apikey);
+        paramMap.put("serial", serial);
+        for (String key : params.keySet()) {
+            paramMap.put(key, params.get(key));
+        }
 
-        final KrollObject thisObject = this.getKrollObject();
-        HTTP_CLIENT.get(getAbsoluteURL(relativePath), requestParams, new JsonHttpResponseHandler() {
-            @Override
-            public void onFailure(Throwable e, JSONObject errorResponse) {
-                if (callback != null) {
-                    callback.callAsync(thisObject, new Object[] { AppersonlabsIoBridgeModule.throwableToErrorDict(e), KrollDict.fromJSON(errorResponse) });
-                }
-            }
-
-            @Override
-            public void onSuccess(JSONObject response) {
-                if (callback != null) {
-                    callback.callAsync(thisObject, new Object[] { null, KrollDict.fromJSON(response) });
-                }
-            }
-        });
-
+        HTTP_CLIENT.get(relativePath, paramMap, new KrollAsyncCallback(this.getKrollObject(), callback));
     }
 
     private void sendPOSTRequest(final String relativePath, final Map<String, String> params, final KrollFunction callback) {
-        try {
-            final KrollObject thisObject = this.getKrollObject();
-            JSONObject obj = new JSONObject(params);
-            HTTP_CLIENT.post(getActivity().getApplicationContext(), getAbsoluteURL(relativePath), new StringEntity(obj.toString()), "application/json",
-                    new JsonHttpResponseHandler() {
-                        @Override
-                        public void onFailure(Throwable e, JSONObject errorResponse) {
-                            if (callback != null) {
-                                callback.callAsync(thisObject,
-                                        new Object[] { AppersonlabsIoBridgeModule.throwableToErrorDict(e), KrollDict.fromJSON(errorResponse) });
-                            }
-                        }
+        JSONObject obj = new JSONObject(params);
 
-                        @Override
-                        public void onSuccess(JSONObject response) {
-                            if (callback != null) {
-                                callback.callAsync(thisObject, new Object[] { null, KrollDict.fromJSON(response) });
-                            }
-                        }
-                    });
-        }
-        catch (UnsupportedEncodingException e) {
-            Log.e(LCAT, e.getMessage());
-        }
+        // POST requests use the X-APIKEY header, so no need to add apikey and
+        // serial number to body
+        HTTP_CLIENT.post(relativePath, "application/json", obj.toString().getBytes(), new KrollAsyncCallback(this.getKrollObject(), callback));
     }
 
     @Kroll.method(name = "writeGPIORegister")
-    public void writeGPIORegister(int channel, String registerName, String content, KrollFunction callback) {
+    public void writeGPIORegister(int channel, String registerName, String content, @Kroll.argument(optional = true) KrollFunction callback) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("serial", serial);
         params.put("channel", String.valueOf(channel));
@@ -145,4 +167,5 @@ public class DeviceProxy extends KrollProxy {
         params.put("content", content);
         sendPOSTRequest(PATH_WRITE_GPIO_REGISTER, params, callback);
     }
+
 }
